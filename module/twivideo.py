8d3cb818b411a2849b723ce20e5a78adcc1048ae
Dotinkasra <tyekepon@pm.me>
2023-11-07 03:21:38 +0900

    updated : URL acquisition method

--- module/twivideo.py
+++ module/twivideo.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 from module.module import Modules
 from schema.db import DB
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome import service
 import time
 import datetime
 
@@ -14,58 +16,22 @@ class Twivideo():
             "https://twivideo.net/?ranking&sort=week"
         ]
 
-    def __get_dynamic_site_html(self, url):
-        driver_path = "/Users/twaoi/bin/chromedriver"
-        browser_path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-
-        options = webdriver.ChromeOptions()
-        options.binary_location = browser_path
-        options.add_argument('--headless')
-
-        driver = webdriver.Chrome(executable_path=driver_path, options = options)
-        driver.get(url)
-        time.sleep(3)
-
-        return driver.page_source.encode('utf-8')
-
-    def __createbs(self, html) -> BeautifulSoup:
-        try:
-            soup = BeautifulSoup(html, 'lxml')
-            return soup
-        except Exception as e:
-            print(e)
-            print("エラー")
-            return None
-
-    def __get_all_video_link(self, bs: BeautifulSoup) -> list[str]:
-        try:
-            div = bs.find("div", attrs={"class": "grids"})
-            a_link = div.find_all("a", attrs={"class": "item_clk item_link"})
-            hrefs = [a.get('href') for a in a_link]
-            return hrefs
-        except Exception as e:
-            print(e)
-            return []
-        
-    def __get_download_video_link(self) -> list[str]:
-        db: DB = DB()
-        urls: list[str] = []
-        for url in self.urls:
-            converted_html: BeautifulSoup = self.__createbs(self.__get_dynamic_site_html(url))
-            if not converted_html: continue
-            for video in self.__get_all_video_link(converted_html):
-                if db.check_url_exists(video):
-                    print("Twivideo : スキップします：{}".format(video))
-                    continue
-                urls.append(video)
-        del db
-        return urls
-
     def do(self):
         print("実行します:{}".format(datetime.datetime.now()))
 
+        ret_links_from_webd = []
+        for url in self.urls:
+            webd = self.TwivideoDlWithWebDriver(url)
+            ret_links_from_webd.append(webd.get_all_video_link())
+
+        video_links = [
+            video_link 
+            for ret_link in ret_links_from_webd
+            for video_link in ret_link 
+        ]
+
         db: DB = DB()
-        for url in self.__get_download_video_link():
+        for url in video_links:
             try:
                 Modules.download_mp4(url)
                 db.insert_single_url(url)
@@ -74,8 +40,48 @@ class Twivideo():
                 print(e)
             finally:
                 continue
-
+            
     def test(self):
         self.do()
+
+    class TwivideoDlWithWebDriver():
+        def __init__(self, url: str) -> None:
+            driver_path = "/Users/twaoi/bin/chromedriver"
+            browser_path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+
+            options = webdriver.ChromeOptions()
+            options.binary_location = browser_path
+
+            download_path = "/Users/twaoi/Server/schedule_app/movie"
+            options.add_experimental_option("prefs", {"download.default_directory": download_path})
+
+            self.driver = webdriver.Chrome(executable_path=driver_path, options = options)
+            self.driver.get(url)
+
+            time.sleep(10)
+
+        def get_all_video_link(self) -> list[str]:
+            grids = self.driver.find_elements(By.CSS_SELECTOR, "div.grids a.item_clk.item_link")
+            urls = []
+            for i in grids:
+                print("実行")
+                try:
+                    i.click()
+                    time.sleep(5)
+                    
+                    if len(self.driver.window_handles) > 1:
+                        self.driver.switch_to.window(self.driver.window_handles[-1])
+                        urls.append(self.driver.current_url)
+                        self.driver.close()
+                        self.driver.switch_to.window(self.driver.window_handles[0])
+                except Exception as e:
+                    print(e)
+                finally:
+                    continue
+            self.driver.close()
+            return urls
+    
+
+
 
 
