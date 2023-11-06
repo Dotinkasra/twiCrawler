d59ebbe5f9a81fa35f1563813f42634fcc136529
Dotinkasra <tyekepon@pm.me>
2023-11-07 03:19:23 +0900

    change : Database using for sqlite3 to MariaDB

--- schema/db.py
+++ schema/db.py
@@ -1,8 +1,12 @@
 from module.module import Modules
-import sqlite3
+import pymysql.cursors
+from dotenv import load_dotenv
+from os.path import join, dirname
+import os
+import datetime
 
 class DB():
-    def __init__(self, dbname: str = 'twidouga.db') -> None:
+    def __init__(self, dbname: str = 'url_records') -> None:
         self.dba = DBaccess(dbname)
 
     def insert_single_url(self, url: str):
@@ -13,14 +17,15 @@ class DB():
         if not video_id:
             return
         
+        now = datetime.datetime.now()
+        
         self.dba.insert(
             sql =
             '''
             INSERT INTO 
-            url_records (video_id, url) VALUES (?, ?);
+            url_records (video_id, created_at, url) VALUES (%s, %s, %s);
             ''',
-            parameta = (video_id, url,),
-            table_name = "url_records"
+            parameta = (video_id, now, url,),
         )
 
     def check_url_exists(self, url: str) -> bool:
@@ -35,7 +40,7 @@ class DB():
             '''
             SELECT count(*)
             FROM url_records
-            WHERE video_id = ?;
+            WHERE video_id = %s;
             ''', (video_id,)
         )
 
@@ -43,66 +48,61 @@ class DB():
 
 class DBaccess():
     def __init__(self, dbname) -> None:
-        self.dbname = dbname
+        load_dotenv(verbose=True)
+        dotenv_path = join(dirname(__file__), '.env')
+        load_dotenv(dotenv_path)
 
-        # SQLiteデータベースに接続
-        conn = sqlite3.connect(self.dbname)
+        try:
+            self.conn = pymysql.connect(
+                host = os.environ.get("MARIADB_HOST"),
+                user = os.environ.get('MARIADB_USER'),
+                db = os.environ.get("MARIADB_DATABASE"),
+                password = os.environ.get("MARIADB_PASSWORD"),
+                charset="utf8mb4",
+                cursorclass=pymysql.cursors.Cursor
+            )
 
-        # cursorオブジェクトを作成
-        cursor = conn.cursor()
-
-        # テーブルを作成するSQL文を実行
-        cursor.execute('''
-        CREATE TABLE IF NOT EXISTS url_records (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        url TEXT UNIQUE NOT NULL,
-        created_at DATETIME DEFAULT (datetime('now', 'localtime'))
-        );
-        ''')
-
-        # 変更をコミット
-        conn.commit()
-
-        # 接続を閉じる
-        conn.close()
-
-    def close(self):
-        self.conn.close()
-
-    def connect(self):
-        self.conn = sqlite3.connect(self.dbname)
-        self.cur = self.conn.cursor()
+            self.table = dbname
+        except Exception as e:
+            print("close")
+            print(e)
+            self.conn.close()
 
     def select(self, sql: str, parameta: tuple = None) -> list:
-        self.connect()
-        if parameta:
-            self.cur.execute(sql, parameta)
-        else:
-            self.cur.execute(sql)
-        result = self.cur.fetchall()
-        self.close()
-        return result
+        try:
+            print("SELECT文")
+            print(sql)
+            with self.conn.cursor() as cursor:
+                cursor.execute(sql, parameta)
+                return cursor.fetchall()
+        except Exception as e:
+            print("close")
+            print(e)
+            self.conn.close()
+        
 
-    def insert(self, sql: str, parameta: tuple = None, table_name: str = None) -> None|list:
-        self.connect()
-        if parameta:
-            self.cur.execute(sql, parameta)
-        else:
-            self.cur.execute(sql)
-        self.conn.commit()
+    def insert(self, sql: str, parameta: tuple = None) -> None|list:
+        try:
+            print("INSERT文")
+            print(sql)
+            print(parameta)
+            with self.conn.cursor() as cursor:
+                cursor.execute(sql, parameta)
+        except Exception as e:
+            print("close")
+            print(e)
+            self.conn.close()
 
-        result = None
-        if table_name:
-            self.cur.execute(f"SELECT * FROM {table_name} WHERE rowid = last_insert_rowid();")
-            result = self.cur.fetchall()
-        self.close()
-        return result
-
-    def delete(self, sql: str, parameta: tuple = None):
-        self.connect()
-        if parameta:
-            self.cur.execute(sql, parameta)
-        else:
-            self.cur.execute(sql)
-        self.conn.commit()
-        self.close()
+        try:
+            self.conn.commit()
+            print("コミット完了後SELECT")
+            with self.conn.cursor() as cursor:
+                cursor.execute("SELECT last_insert_id() FROM url_records")
+                return cursor.fetchall()
+        except Exception as e:
+            print("close")
+            print(e)
+            self.conn.close()
+    
+    def __del__(self):
+        self.conn.close()
